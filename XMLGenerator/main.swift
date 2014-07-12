fd913814e8e31030e51b56f42780e4a8d04a3801
Mr.Yello <ibessa@joinmypin.com>
2014-07-11 21:13:07 -0400

    Initial Commit

---
+++ XMLGenerator/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  XMLGenerator
+//
+//  Created by iury bessa on 7/11/14.
+//  Copyright (c) 2014 yellokrow. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
