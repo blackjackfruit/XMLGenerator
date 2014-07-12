5fccc120102543510075c1bd465177495695638f
Mr.Yello <ibessa@joinmypin.com>
2014-07-11 23:38:29 -0400

    Initial Commit

---
+++ XMLGenerator/XMLGenerator.swift
@@ -0,0 +1,85 @@
+//
+//  XMLGenerator.swift
+//  XMLGenerator
+//
+//  Created by iury bessa on 7/11/14.
+//  Copyright (c) 2014 yellokrow. All rights reserved.
+//
+
+import Foundation
+
+class XMLGenerator {
+    
+    var xmlString : NSMutableString = "";
+    var tags:Array<String> = [];
+    let ELEMENT = ""
+    
+    init()
+    {
+        xmlString.appendString("<?xml version=\"1.0\" encoding=\"UTF-8\"?>")
+    }
+    
+    init(ver:Double,encode:NSString)
+    {
+        xmlString.appendString("<?xml version=\"\(ver)\" encoding=\"\(encode)\"?>")
+    }
+    
+    func tag(tagName:NSString)
+    {
+        assert(tagName != ELEMENT, "CANNOT HAVE TAGNAME TO BE EMPTY")
+        
+        xmlString.appendString("<\(tagName)>")
+        tags.append(tagName)
+    }
+    
+    func tagAttributes(attributes:NSDictionary)
+    {
+        var tempXMLAttributes: NSMutableString = ""
+        let keys = attributes.allKeys
+        let lastIndex = keys.count - 1
+        var counter = 0
+        for k in keys {
+            let value:NSString = attributes.objectForKey(k) as NSString
+            if(counter != lastIndex)
+            {
+                tempXMLAttributes.appendString("\(k)=\"\(value)\" ")
+            }
+            else
+            {
+                tempXMLAttributes.appendString("\(k)=\"\(value)\"")
+            }
+            counter++;
+        }
+        
+        let range = NSRange(location: xmlString.length - 1, length: 1)
+        xmlString = NSMutableString(string: xmlString.stringByReplacingCharactersInRange(range, withString: " "))
+        xmlString.appendString("\(tempXMLAttributes)>")
+    }
+    func addElement(input: NSString)
+    {
+        xmlString.appendString("\(input)</\(tags[tags.count-1])>")
+        tags.removeLast()
+    }
+    
+    func end()
+    {
+        assert(tags.count > 0, "Can't remove tag when there aren't any")
+        
+        let range = NSRange(location: xmlString.length - 1, length: 1)
+        xmlString = NSMutableString(string: xmlString.stringByReplacingCharactersInRange(range, withString: "/>"))
+        tags.removeLast()
+    }
+    
+    func endTag()
+    {
+        assert(tags.count > 0, "Can't remove tag when there aren't any")
+        
+        xmlString.appendString("</\(tags[tags.count-1])>")
+        tags.removeLast()
+    }
+    
+    func output() -> String
+    {
+        return xmlString
+    }
+}
---
+++ XMLGeneratorTests/XMLGeneratorTests.swift
@@ -0,0 +1,63 @@
+//
+//  XMLGeneratorTests.swift
+//  XMLGeneratorTests
+//
+//  Created by iury bessa on 7/11/14.
+//  Copyright (c) 2014 yellokrow. All rights reserved.
+//
+
+import Cocoa
+import XCTest
+import XMLGenerator
+
+class XMLGeneratorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testBasicStructure()
+    {
+        let xml = XMLGenerator(ver: 1.0, encode: "UTF-8");
+        xml.tag("Library");
+        xml.tagAttributes(["address":"0000 Candy Land Drive"]);xml.end()
+            xml.tag("catalog")
+                xml.tag("book")
+                xml.tagAttributes(["id":"0000"])
+                    xml.tag("author")
+                    xml.addElement("Steven King") //The closing tag is automatically added
+                xml.endTag()
+                xml.tag("book")
+                xml.tagAttributes(["id":"0001"])
+                    xml.tag("author")
+                    xml.addElement("Paulo Coehlo") //The closing tag is automatically added
+                xml.endTag()
+        xml.endTag()
+        
+        let out = xml.output()
+        println(out);
+    }
+    
+    func testEmptyTagName()
+    {
+        let xml = XMLGenerator()
+        xml.tag("")
+        xml.endTag()
+        let out = xml.output()
+        println(out)
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
